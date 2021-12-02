ab6c02e112b2d495c1a7393a5deddf381323c4e6
Ben-Lichtman <Ben_Lichman@icloud.com>
2021-12-01 22:02:51 -0800

    big improvements to attributes

--- parst_derive/src/attributes.rs
+++ parst_derive/src/attributes.rs
@@ -1,5 +1,5 @@
 use quote::ToTokens;
-use syn::{Attribute, Lit, LitInt, Meta, MetaNameValue, NestedMeta, Type};
+use syn::{Attribute, Expr, Lit, LitInt, Meta, MetaNameValue, NestedMeta, Type};
 
 #[derive(Debug)]
 pub struct OuterAttributes {
@@ -10,26 +10,25 @@ pub fn parse_outer_attributes(input: &[Attribute]) -> OuterAttributes {
 	let mut outer_attributes = OuterAttributes { context: None };
 
 	input
-		.into_iter()
+		.iter()
 		.flat_map(|a| {
 			let args = a.parse_args::<NestedMeta>().ok()?;
 			Some((a.path.clone(), args))
 		})
-		.filter(|(path, _)| path.to_token_stream().to_string() == "parst")
-		.for_each(|(_, meta)| match meta {
-			NestedMeta::Meta(Meta::NameValue(MetaNameValue {
+		.filter(|(path, _)| path.into_token_stream().to_string() == "parst")
+		.for_each(|(_, meta)| {
+			if let NestedMeta::Meta(Meta::NameValue(MetaNameValue {
 				path,
 				lit: Lit::Str(litstr),
 				..
-			})) => match path.into_token_stream().to_string().as_str() {
-				"context" => {
+			})) = meta
+			{
+				if path.into_token_stream().to_string().as_str() == "context" {
 					if let Ok(c) = litstr.parse::<Type>() {
 						outer_attributes.context = Some(c);
 					}
 				}
-				_ => (),
-			},
-			_ => (),
+			}
 		});
 
 	outer_attributes
@@ -37,48 +36,50 @@ pub fn parse_outer_attributes(input: &[Attribute]) -> OuterAttributes {
 
 #[derive(Debug)]
 pub struct InnerAttributes {
-	pub with_context: bool,
+	pub with_context: InnerContext,
 	pub assert_eq: Option<LitInt>,
 	pub assert_ne: Option<LitInt>,
 }
 
+#[derive(Debug)]
+pub enum InnerContext {
+	None,
+	Parent,
+	Field(Box<Expr>),
+}
+
 pub fn parse_inner_attributes(input: &[Attribute]) -> InnerAttributes {
 	let mut inner_attributes = InnerAttributes {
-		with_context: false,
+		with_context: InnerContext::None,
 		assert_eq: None,
 		assert_ne: None,
 	};
 
-	input
-		.into_iter()
-		.flat_map(|a| {
-			let args = a.parse_args::<NestedMeta>().ok()?;
-			Some((a.path.clone(), args))
-		})
-		.for_each(|(path, meta)| {
-			let path_str = path.into_token_stream().to_string();
-			match path_str.as_str() {
-				"parst" => {
-					if let NestedMeta::Meta(Meta::Path(path)) = meta {
-						let path_str = path.into_token_stream().to_string();
-						if path_str.as_str() == "with_context" {
-							inner_attributes.with_context = true;
-						}
-					}
-				}
-				"assert_eq" => {
-					if let NestedMeta::Lit(Lit::Int(l)) = meta {
-						inner_attributes.assert_eq = Some(l);
-					}
-				}
-				"assert_ne" => {
-					if let NestedMeta::Lit(Lit::Int(l)) = meta {
-						inner_attributes.assert_ne = Some(l);
-					}
-				}
-				x => panic!("{:#?}", x),
+	input.iter().for_each(|a| {
+		let path_str = a.path.to_token_stream().to_string();
+		match path_str.as_str() {
+			"with_context" => {
+				inner_attributes.with_context = InnerContext::Parent;
 			}
-		});
+			"with_field_context" => {
+				if let Ok(NestedMeta::Lit(Lit::Str(l))) = a.parse_args::<NestedMeta>() {
+					let expr = l.parse().unwrap();
+					inner_attributes.with_context = InnerContext::Field(expr);
+				}
+			}
+			"assert_eq" => {
+				if let Ok(NestedMeta::Lit(Lit::Int(l))) = a.parse_args::<NestedMeta>() {
+					inner_attributes.assert_eq = Some(l);
+				}
+			}
+			"assert_ne" => {
+				if let Ok(NestedMeta::Lit(Lit::Int(l))) = a.parse_args::<NestedMeta>() {
+					inner_attributes.assert_ne = Some(l);
+				}
+			}
+			x => panic!("{:#?}", x),
+		}
+	});
 
 	inner_attributes
 }
