bf4abc4ee1a1da069a3b01a4beaa4453e413de88
Ben-Lichtman <Ben_Lichtman@icloud.com>
2021-12-01 22:02:51 -0800

    big improvements to attributes

--- parst_derive/src/lib.rs
+++ parst_derive/src/lib.rs
@@ -6,7 +6,10 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-#[proc_macro_derive(Parsable, attributes(parst, assert_eq, assert_ne))]
+#[proc_macro_derive(
+	Parsable,
+	attributes(parst, assert_eq, assert_ne, with_context, with_field_context)
+)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	let derive_input = parse_macro_input!(input as DeriveInput);
 	proc_macro::TokenStream::from(process_input(&derive_input))
@@ -18,7 +21,7 @@ fn process_input(input: &DeriveInput) -> TokenStream {
 
 	let outer_attributes = parse_outer_attributes(&input.attrs);
 
-	let expression = generate_expression(&input);
+	let expression = generate_expression(input);
 
 	match outer_attributes.context {
 		None => {
@@ -27,26 +30,18 @@ fn process_input(input: &DeriveInput) -> TokenStream {
 				impl<'a, C> ::parst::Parsable<'a, C> for #ident #generics
 					where C: Copy,
 				{
-					fn read(bytes: &'a [u8], _context: C) -> ::parst::PResult<'a, Self> {
+					fn read(__bytes: &'a [u8], __context: C) -> ::parst::PResult<'a, Self> {
 						#![allow(non_snake_case)]
 						#expression
 					}
 				}
 			}
-			// quote! {
-			// 	impl<'a> ::parst::Parsable<'a, ()> for #ident #generics {
-			// 		fn read(bytes: &'a [u8], _context: ()) -> ::parst::PResult<'a, Self> {
-			// 			#![allow(non_snake_case)]
-			// 			#expression
-			// 		}
-			// 	}
-			// }
 		}
 		Some(t) => {
 			// Implement for some specific C
 			quote! {
 				impl<'a> ::parst::Parsable<'a, #t> for #ident #generics {
-					fn read(bytes: &'a [u8], _context: #t) -> ::parst::PResult<'a, Self> {
+					fn read(__bytes: &'a [u8], __context: #t) -> ::parst::PResult<'a, Self> {
 						#![allow(non_snake_case)]
 						#expression
 					}
